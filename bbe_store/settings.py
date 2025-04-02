22bf7f9c6eaab6c9f6fb93b9876e263898396e2f
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2025-04-02 11:52:00 +0100

    Install django, migrate, update settings.py

--- bbe_store/settings.py
+++ bbe_store/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-05opb)$$(&$%-k^hfzra8rp^cr4!z^x*9t%_)*i^1ezbk@27o1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
