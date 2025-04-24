db3775b1a9f6ac1ecaf07d97ef389e4ce345b984
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2025-04-24 19:51:48 +0100

    Add allauth and base templates

--- bbe_store/settings.py
+++ bbe_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-05opb)$$(&$%-k^hfzra8rp^cr4!z^x*9t%_)*i^1ezbk@27o1
 # SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
