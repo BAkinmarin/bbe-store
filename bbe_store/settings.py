e45a3fe68f6094357652e3db9e40b4c09a1a38cc
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2025-05-26 20:08:31 +0100

    Add products view

--- bbe_store/settings.py
+++ bbe_store/settings.py
@@ -98,10 +98,12 @@ SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
-ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+# ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+# ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+
+ACCOUNT_LOGIN_METHODS = {'username', 'email'}
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'email2*', 'username*', 'password1*', 'password2*']
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
