deaa2800b257923ff7f07ee3d82a290df97cd987
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2025-06-18 12:54:38 +0100

    Add Stripe for payments, integrate elements, add styles

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RbJqlIx6wkpjSguzcgBHobx0BXVSQ218ifzzRbher12jBOyFEMnOXHRbtuaeXzJhQyPHzccLXkWcPgdRcFyBlPB00yGdsZlNB',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
