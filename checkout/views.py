6be3ad1780daefb87a196cdfbe783e99fc144f38
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2025-06-18 14:33:55 +0100

    Install Stripe, add secret key to env, update checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 # Create your views here.
 
@@ -11,11 +15,15 @@ def checkout(request):
         messages.error(request, "There's nothing in your shopping basket")
         return redirect(reverse('products'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51RbJqlIx6wkpjSguzcgBHobx0BXVSQ218ifzzRbher12jBOyFEMnOXHRbtuaeXzJhQyPHzccLXkWcPgdRcFyBlPB00yGdsZlNB',
+        'stripe_public_key': 'pk_live_51RbJqlIx6wkpjSguSUKdGr4o4wacvKoUpVqKJvSeJLPqKcQkyhYj9XSOgoXFOT7F6M9g7YdzI612o9WiG2klCmTG003wITVEd7',
         'client_secret': 'test client secret',
     }
 
