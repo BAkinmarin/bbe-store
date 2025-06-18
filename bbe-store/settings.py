a8859f882ecb91132e8759dcacf7083ee11de0f2
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2025-06-18 19:51:58 +0100

    Set MEDIA_ROOT and STATIC_ROOT for Heroku

--- bbe-store/settings.py
+++ bbe-store/settings.py
@@ -174,6 +174,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
@@ -183,6 +184,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Calculate delivery cost
 FREE_DELIVERY_THRESHOLD = 50
