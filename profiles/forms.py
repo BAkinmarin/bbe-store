1d2ed45880b35a85be10c59d247384686becb52a
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2025-09-28 19:00:46 +0100

    Amend allauth logout.html and checkout_success.html

--- profiles/forms.py
+++ profiles/forms.py
@@ -14,15 +14,15 @@ class UserProfileForm(forms.ModelForm):
         """
         super().__init__(*args, **kwargs)
         placeholders = {
-            'default_phone_number': 'Phone Number',
             'default_street_address1': 'Street Address 1',
             'default_street_address2': 'Street Address 2',
             'default_town_or_city': 'Town or City',
             'default_postcode': 'Postal / Zip Code',
             'default_county': 'County / State',
+            'default_phone_number': 'Phone Number',
         }
 
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        self.fields['default_street_address1'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'default_country':
                 if self.fields[field].required:
