2ca7f1ddb4b4b017b9656f3ac7a2339dcf72438b
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2025-09-28 21:49:24 +0100

    Add webhook handler

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -44,7 +44,7 @@ card.addEventListener('change', function (event) {
     }
 });
 
-// Handle paymnet submission
+// Handle payment submission
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
