6609e94f1b3c24244a9f38972347751bf2cdf2bf
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2026-01-02 08:39:39 +0000

    Add Product Admin to Products forms.py

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,24 @@
 from django import forms
 from .models import Review
+from .models import Product, Category
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['rating', 'review_text',]
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget_attrs['class'] = 'border-black rounded-0'
