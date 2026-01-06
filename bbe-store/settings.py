0719a90873208c680e30c6221198b3f2856e2610
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2026-01-06 11:00:49 +0000

    Finalise set up for receiving real emails, and update README

--- bbe-store/settings.py
+++ bbe-store/settings.py
@@ -220,8 +220,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.email.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'bbe@example.com'
-    # DEFAULT_FROM_EMAIL = 'bbe-ecommerce-store@example.com'
+    DEFAULT_FROM_EMAIL = 'bbe-ecommerce-store@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
